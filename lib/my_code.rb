952cf03341d796520069cbf8871643b8ef6e674b
virajransing <virajransing@gmail.com>
2019-08-24 17:57:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,14 @@ end
 new
 end 
  
-def reduce(array, )
-  
+def reduce(array, sv = nil )
+  if sv
+    num1 = sv
+    i = 0 
+  else 
+    num1 = array[0]
+    i = num1
+  end 
+  while i < array.length 
   
 end 
