94c6f1757595c7d0b70db7b6e52bb343cbbe84fe
virajransing <virajransing@gmail.com>
2019-08-24 17:45:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,25 +1,11 @@
 def map(array)
-  #map returns a new array of manupulated elements
   new = []
   i = 0 
   while i < array.length 
-    new.push(yield(array[i]))
-    i += 1 
-  end 
-  new 
+  new.push(yield(array[i]))
+  i += 1 
 end 
-
-def reduce(array, sv=nil)
-  if sv 
-    sum = sv
-    i=0 
-  else 
-    sum = array[0]
-    i = 1 
-  end 
-  while i < array.length
-    sum = yield(sum, array[i])
-    i += 1 
-  end 
-  sum 
+new
 end 
+ 
+ 
